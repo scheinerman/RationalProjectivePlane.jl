a8bab571a191428f2a81e4310b4ad9e9b373c6ee
Ed Scheinerman <ers@jhu.edu>
2023-11-19 16:40:30 -0500

    Incidence methods

--- src/meet_join.jl
+++ src/meet_join.jl
@@ -1,17 +1,18 @@
 function _op1(a::HVector, b::HVector)::Vector
-    if a==b
+    if a == b
 
     end
-    if a==b
+    if a == b
         throw(ArgumentError("Two arguments cannot be the same"))
     end
-    A = [ a.data'; b.data' ]
+    A = [a.data'; b.data']
 
     N = nullspacex(A)
-    return N[:,1] 
+    return N[:, 1]
 end
 
 import AbstractLattices: ∨, ∧
+export two_lines, two_points, ∨, ∧
 
 
 """
@@ -33,4 +34,33 @@ Given lines `a` and `b`, return the point at which they intersect.
 function ∧(a::PLine, b::PLine)::PPoint
     v = _op1(a.data, b.data)
     return PPoint(v)
+end
+
+
+function _op2(a::HVector)
+    M = reshape(a.data, 1, 3)
+    N = nullspacex(M)
+    v = N[:, 1]
+    w = N[:, 2]
+    return v, w
+end
+
+"""
+    two_points(L::PLine)::Tuple{PPoint,PPoint}
+
+Given a line `L` return a pair of distinct points on `L`.
+"""
+function two_points(L::PLine)::Tuple{PPoint,PPoint}
+    v, w = _op2(L.data)
+    return PPoint(v), PPoint(w)
+end
+
+"""
+    two_lines(a::PPoint)::Tuple{PLine, PLine}
+
+Given a point `a` return a pair of distinct lines that contain `a`.
+"""
+function two_lines(a::PPoint)::Tuple{PLine,PLine}
+    v, w = _op2(a.data)
+    return PLine(v), PLine(w)
 end
