e599429d2578cef0422e28dd0578a37f40889459
Ed Scheinerman <ers@jhu.edu>
2023-11-20 17:32:36 -0500

    Crude drawing

--- src/drawing.jl
+++ src/drawing.jl
@@ -2,7 +2,7 @@ using SimpleDrawing
 
 import SimpleDrawing: draw
 
-const _ppoint_size = 4
+const _ppoint_size = 5
 
 function draw(a::PPoint)
     if isinf(a)
@@ -22,9 +22,27 @@ function draw(L::PLine)
 end
 
 
-function draw(objs::Vector{ProjectiveObject})
-    points = [it for it ∈ objs if it isa PPoint]
-    lines = [it for it ∈ objs if it isa PLine]
+function draw(objs::Vector{T}) where {T<:ProjectiveObject}
+    points = [it for it ∈ objs if it isa PPoint && !isinf(it)]
+    lines = [it for it ∈ objs if it isa PLine && !isinf(it)]
+
+    if length(points) > 0
+        xylist = cartesian.(points)
+        xlist = first.(xylist)
+        ylist = last.(xylist)
+
+        xmin = minimum(xlist)
+        xmax = maximum(xlist)
+
+        ymin = minimum(ylist)
+        ymax = maximum(ylist)
+
+        draw_point(xmin, ymin, marker = 0)
+        draw_point(xmax, ymax, marker = 0)
+        p,q = corners()
+        draw_point(p,marker=0)
+        draw_point(q,marker=0)
+    end
 
     for p ∈ points
         draw(p)
