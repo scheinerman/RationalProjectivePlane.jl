b3114758929cabf72f0d529ad20e851a92975c3e
Ed Scheinerman <ers@jhu.edu>
2023-11-22 07:51:59 -0500

    new example

---
+++ examples/grid.jl
@@ -0,0 +1,20 @@
+using RationalProjectivePlane, Clines, Plots, SimpleDrawing
+
+"""
+    grid(n::Int = 3)
+
+Return a list of points and lines in which the points form an `n`-by-`n`
+grid and the `2n` lines run horizontally and vertically through the grid.
+"""
+function grid(n::Int = 3)
+    if n < 2
+        throw(ArgumentError("Argument must be at least 2"))
+    end
+
+    plist = [PPoint(i, j, 1) for i = 1:n for j = 1:n]
+
+    lines1 = [PPoint(i, 1, 1) ∨ PPoint(i, n, 1) for i = 1:n]
+    lines2 = [PPoint(1, i, 1) ∨ PPoint(n, i, 1) for i = 1:n]
+
+    return [plist; lines1; lines2]
+end
